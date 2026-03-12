854e6fb1eef5633ed400aa34b3330b1e54fae78e
henry2craftman <henry2craftman@gmail.com>
2026-03-12 15:17:51 +0900

    260312 - Physics Material 까지

---
+++ Assets/Scripts/CylinderSensor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CylinderSensor : MonoBehaviour
+{
+    public bool isDetected;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "금속")
+            isDetected = true;
+    }
+}
--- Assets/Scripts/CylinderSystem.cs
+++ Assets/Scripts/CylinderSystem.cs
@@ -1,14 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class CylinderSystem : MonoBehaviour
 {
-    public Sensor sensor;
+    public CylinderSensor sensor;
     public Transform pusher;
+    public Transform target;
+    public float pusherSpeed = 0.5f;
+    bool isForward = true;
+    Vector3 originPos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        originPos = pusher.position;
     }
 
     // Update is called once per frame
@@ -17,6 +22,36 @@ public class CylinderSystem : MonoBehaviour
         if(sensor.isDetected)
         {
             // 푸셔 이동...
+            sensor.isDetected = false;
+
+            if(isForward)
+            {
+                StartCoroutine(MovePusherTo(target.position));
+            }
+            else
+            {
+                StartCoroutine(MovePusherTo(originPos));
+            }
+
+        }
+    }
+
+    IEnumerator MovePusherTo(Vector3 to)
+    {
+        while(true)
+        {
+            Vector3 dir = to - pusher.position;
+            float distance = dir.magnitude;
+
+            if(distance < 0.01f)
+            {
+                isForward = !isForward;
+                break;
+            }
+
+            pusher.position += dir * pusherSpeed * Time.deltaTime;
+
+            yield return null;
         }
     }
 }
